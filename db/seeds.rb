fdbc416c4fc838767d4665fc5d8d14ea7ce2d8b1
Komenpurity <jepkorirpurity8@gmail.com>
2023-07-31 10:31:11 +0300

    added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+
+Product.create(name: "Onions",category: "Vegetables",description: "Red onions,white onions", price: 20, image: "https://images.unsplash.com/photo-1587049633312-d628ae50a8ae?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8b25pb258ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60")
+Reviews.create(user_id: 1, rider_id:1, description: "Very impressed,delivered on time and in good state")
+Rider.create(name: "Michael Jordan") 
