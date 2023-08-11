b9648af2e575a5ed587bb578d413e725207f4d16
danieljuma5 <jumadaniel275@gmail.com>
2023-08-11 12:53:26 +0300

    Fixing cors issues

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -4,7 +4,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3001', %r{https://.*\.netlify\.app/}
+    origins 'http://localhost:3001', %r{https://.*\.netlify\.app}
     resource '*', headers: :any, methods: %i[get post patch put delete]
   end
 end
